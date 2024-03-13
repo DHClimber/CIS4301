88fe09a204f61e01e86a701ac2ae13a2f384a60f
lucindaq <lucinda@merl.us>
2024-03-13 13:42:47 +0000

    adds custom dashboard view page views

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,64 +1,15 @@
 'use client'
-import { DashboardTile } from '@/lib/types'
-import { Button, Card, Grid, Stack, Typography } from '@mui/material'
+import { Button, Card, Grid, Link, Stack, Typography } from '@mui/material'
 import { Bar, BarChart, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { testTiles } from '../test-data';
 
 export default async function Dashboard() {
-  const tiles: DashboardTile[] = [
-    {id: '1', title: 'Crashes per day', data: data}, 
-    {id: '2', title: 'Age for crashes', data: data}
-  ];
 
   return (
-    <Stack padding={5} spacing={1}>
+    <Stack spacing={1}>
       <Typography variant='h3'>Dashboard</Typography>
       <Grid container spacing={1}>
-        {tiles.map((tile) => (
+        {testTiles.map((tile) => (
           <Grid item xs={3}>
             <Card sx={{padding: 5}}>
               <Typography variant='h6'>{tile.title}</Typography>            
@@ -67,7 +18,9 @@ export default async function Dashboard() {
                   <Bar dataKey="uv" fill="#8884d8" />
                 </BarChart>
               </ResponsiveContainer>
-              <Button>See more</Button>
+              <Link href={`/dashboard/${tile.id}`}>
+                <Button>See more</Button>
+              </Link>
             </Card>
           </Grid>
         ))}
