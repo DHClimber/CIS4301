668481b20201a425aac475888196771db8dcf11b
lucindaq <lucinda@merl.us>
2024-04-16 12:59:51 +0200

    Implements FilterControls component in the dashboard page and the individual tile pages

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,33 +1,60 @@
 'use client'
-import { getAllDashboardTiles } from '@/lib/queries';
-import { Button, Card, Grid, Link, Stack, Typography } from '@mui/material'
-import { Bar, BarChart, ResponsiveContainer } from 'recharts';
-// import { testTiles } from '../test-data';
+import { getAllDashboardTiles, getAllFilteredTiles } from '@/lib/queries';
+import { Button, Card, CircularProgress, Grid, Link, Stack, Typography } from '@mui/material'
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import FilterControls from '@/components/shared/filter-controls';
+import { FilterValues, DashboardTile, BarChartDataFormat } from '@/lib/types';
+import { useEffect, useState } from 'react';
+import { generateHexColorFromLetter, getIdToKeysMap } from '@/lib/functions';
 
 export default async function Dashboard() {
+  const [tiles, setTiles] = useState<DashboardTile[]>([]);
+  const [tileIdToChartKeys, setTileIdToChartKeys] = useState<{[id: string]: string[]}>();
 
-  const testTiles = await getAllDashboardTiles("2015-12-25", "2017-12-25");
+  useEffect(() => {
+    (async () => {
+      const dashTiles: DashboardTile[] = await getAllDashboardTiles() || [];
+      setTiles(dashTiles);
+      setTileIdToChartKeys(getIdToKeysMap(dashTiles));
+    })(); 
+  }, []);
+
+  const onFiltersSubmit = async (filters: FilterValues) => {
+    const newDashTiles = await getAllFilteredTiles(filters);
+    setTiles(newDashTiles);
+    getIdToKeysMap(newDashTiles);
+  }
 
   return (
     <Stack spacing={1}>
       <Typography variant='h3'>Dashboard</Typography>
-      <Grid container spacing={1}>
-        {testTiles?.map((tile) => (
-          <Grid item xs={3}>
-            <Card sx={{padding: 5}}>
-              <Typography variant='h6'>{tile.title}</Typography>            
-              <ResponsiveContainer width={250} height={150}>
-                <BarChart width={250} height={150} data={tile.data}>
-                  <Bar dataKey={Object.keys(tile.data[0])[1]} fill="#8884d8" />
-                </BarChart>
-              </ResponsiveContainer>
-              <Link href={`/dashboard/${tile.id}`}>
-                <Button>See more</Button>
-              </Link>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      <Stack direction={'row'} gap={2}>
+        <Grid container spacing={1}>
+          {tileIdToChartKeys ? tiles.map((tile) => (
+            <Grid item xs={3}>
+              <Card sx={{padding: 5}}>
+                <Typography variant='h6'>{tile.title}</Typography>            
+                <ResponsiveContainer width={250} height={150}>
+                  <BarChart width={250} height={150} data={tile.data}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    {tileIdToChartKeys[tile.id].map((key: string) => {
+                      const color = generateHexColorFromLetter(key.charCodeAt(0));
+                      return <Bar dataKey={key} fill={`#${color}`} />
+                    })}
+                  </BarChart>
+                </ResponsiveContainer>
+                <Link href={`/dashboard/${tile.id}`}>
+                  <Button>See more</Button>
+                </Link>
+              </Card>
+            </Grid>
+          )) : (
+            <CircularProgress />
+          )}
+        </Grid>
+        <FilterControls datePicker onSubmit={onFiltersSubmit}/>
+      </Stack>
     </Stack>
   )
 }
